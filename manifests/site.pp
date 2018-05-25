e836a7857e8b344f18ac6a564e81f7571e327f65
Gonzalo Banuelos <gbanuel@gmail.com>
2018-05-25 14:33:15 -0500

    More

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,2 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => "This is my content. Blah!",
-    owner => root,
-  }
-  file { '/root/README':
-    owner => root,
-  }
-
 }
